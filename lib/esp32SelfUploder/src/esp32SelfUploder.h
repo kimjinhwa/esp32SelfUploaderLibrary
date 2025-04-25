90dc770a92db5a9b0fce4eff57c00ebac24950aa
kimjinhwa <mvcjhkim@gmail.com>
2025-04-25 19:35:25 +0900

    완성된 라이브러리.

--- lib/esp32SelfUploder/src/esp32SelfUploder.h
+++ lib/esp32SelfUploder/src/esp32SelfUploder.h
@@ -1,6 +1,15 @@
 #ifndef ESP32SELFUPLODER_H
 #define ESP32SELFUPLODER_H
 
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <HTTPUpdateServer.h>
+#include <Update.h>
+#include <HTTPClient.h>
+#include <WiFiClientSecure.h>
 #include <ArduinoJson.h>
 
 struct FirmwareInfo {
@@ -13,22 +22,24 @@ class ESP32SelfUploder {
         FirmwareInfo currentFirmware;
         static const int FIRMWARE_INFO_ADDR = 0;
         
-        bool checkNewVersion(const char* version_url);
-        
     public:
         char ssid[32];
         char password[32];
-        unsigned int ledPin=15;
+        char update_url[256];
+        WebServer httpServer;
+        HTTPUpdateServer httpUpdater;
+        unsigned int ledPin;
         void setLed(unsigned int pin){
             ledPin = pin;
-            pinMode(ledPin , OUTPUT);
+            pinMode(ledPin, OUTPUT);
         };
-        ESP32SelfUploder() {
+        ESP32SelfUploder() : httpServer(80) {
         }
 
         void begin(const char* ssid, const char* password, const char* update_url);
         void loop();
         bool tryAutoUpdate(const char* firmware_url);
+        bool checkNewVersion(const char* version_url);
 };
 
 extern ESP32SelfUploder selfUploder;
